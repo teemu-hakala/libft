e49dd4376990c13dd89af26b8782e4697796d546
Teemu Hakala <thakala@c1r6p16.hive.fi>
2021-11-01 15:37:23 +0200

    ft_strlen as forty-two function

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,13 +6,13 @@
 /*   By: thakala <thakala@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/31 13:04:18 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/01 15:19:17 by thakala          ###   ########.fr       */
+/*   Updated: 2021/11/01 15:36:57 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	strlen(const char *s)
+size_t	ft_strlen(const char *s)
 {
 	size_t	l;
 
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: thakala <thakala@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/31 12:21:50 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/01 15:07:56 by thakala          ###   ########.fr       */
+/*   Updated: 2021/11/01 15:34:41 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # include <string.h>
 
-size_t	strlen(const char *s);
+size_t	ft_strlen(const char *s);
+void	ft_putchar_fd(char c, int fd);
 
 #endif
