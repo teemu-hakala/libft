a3a34cb55253fc5f15e54c0fb0b23ffdf1563021
Teemu Hakala <thakala@c1r6p16.hive.fi>
2021-11-02 12:01:42 +0200

    ft_putnbr_fd: wrong function name fix

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: thakala <thakala@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 09:30:17 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/01 15:29:34 by thakala          ###   ########.fr       */
+/*   Updated: 2021/11/02 11:51:11 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,14 @@ void	ft_putnbr_fd(int n, int fd)
 	{
 		if (n == (int)(~((~0u) >> 1)))
 		{
-			ft_putchar_fd(-((int)(~((~0u) >> 1) / 10)), fd);
-			ft_putchar_fd(-((int)(~((~0u) >> 1) % 10)), fd);
+			ft_putnbr_fd(-((int)(~((~0u) >> 1) / 10)), fd);
+			ft_putnbr_fd(-((int)(~((~0u) >> 1) % 10)), fd);
 		}
 		else
+		{
+			ft_putchar_fd('-', fd);
 			ft_putnbr_fd(-n, fd);
+		}
 	}
 	else
 	{
