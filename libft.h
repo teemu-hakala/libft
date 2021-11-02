47944c811511aebf1edaad2615699c30055d5a8b
Teemu Hakala <thakala@c1r6p16.hive.fi>
2021-11-02 12:02:21 +0200

    libft.h update ft_putstr_fd parameter type

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: thakala <thakala@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/31 12:21:50 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/01 16:49:53 by thakala          ###   ########.fr       */
+/*   Updated: 2021/11/02 11:49:37 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,6 @@
 
 size_t	ft_strlen(const char *s);
 void	ft_putchar_fd(char c, int fd);
-void	ft_putstr_fd(char *s, int fd);
+void	ft_putstr_fd(char const *s, int fd);
 
 #endif
