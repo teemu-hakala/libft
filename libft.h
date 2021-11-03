3872040a5b91f3849869fbe7c8de9a879c338577
Teemu Hakala <thakala@c1r6p19.hive.fi>
2021-11-03 09:09:04 +0200

    libft ft_ prefixes

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: thakala <thakala@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/31 12:21:50 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/02 11:49:37 by thakala          ###   ########.fr       */
+/*   Updated: 2021/11/03 09:04:46 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,7 @@
 size_t	ft_strlen(const char *s);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char const *s, int fd);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
 
 #endif
