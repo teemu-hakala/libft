48aa3d1195ea82e7f9d4331eca928ad4493687fd
Teemu Hakala <thakala@c1r6p7.hive.fi>
2021-11-04 18:33:01 +0200

    ft_strncpy preliminary

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -6,8 +6,23 @@
 /*   By: thakala <thakala@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 16:37:45 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/04 16:38:16 by thakala          ###   ########.fr       */
+/*   Updated: 2021/11/04 18:27:34 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+char	*ft_strncpy(char *dst, const char *src, size_t len)
+{
+	char	*new_beginning;
+
+	new_beginning = dst;
+	while (*src && len)
+	{
+		*dst++ = *src++;
+		len--;
+	}
+	while (len--)
+		*dst++ = '\0';
+	return (new_beginning);
+}
