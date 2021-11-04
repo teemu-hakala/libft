fb392527ce7224a2d5340f59f40a19de71bb4323
Teemu Hakala <thakala@c1r6p7.hive.fi>
2021-11-04 19:51:37 +0200

    ft_memalloc preliminary

---
+++ ft_memalloc.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memalloc.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thakala <thakala@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/04 19:44:35 by thakala           #+#    #+#             */
+/*   Updated: 2021/11/04 19:51:13 by thakala          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+void	*ft_memalloc(size_t size)
+{
+	char	*block;
+
+	block = (char *)malloc(sizeof(char) * size);
+	if (!block)
+		return (block);
+	while (size)
+		block[size--] = '\0';
+	block[size] = '\0';
+	return (block);
+}
