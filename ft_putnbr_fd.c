8c718036cf59b78b4b16dcd880450c305e5da573
Teemu Hakala <thakala@c1r6p16.hive.fi>
2021-11-04 11:25:14 +0200

    ft_putnbr tested version

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: thakala <thakala@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 09:30:17 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/02 11:51:11 by thakala          ###   ########.fr       */
+/*   Updated: 2021/11/04 11:24:34 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,20 @@ void	ft_putnbr_fd(int n, int fd)
 {
 	if (n < 0)
 	{
-		if (n == (int)(~((~0u) >> 1)))
+		ft_putchar_fd('-', fd);
+		if (n == (int)(~(~0u >> 1)))
 		{
-			ft_putnbr_fd(-((int)(~((~0u) >> 1) / 10)), fd);
-			ft_putnbr_fd(-((int)(~((~0u) >> 1) % 10)), fd);
+			ft_putnbr_fd(-(n / 10), fd);
+			ft_putnbr_fd(-(n % 10), fd);
 		}
 		else
-		{
-			ft_putchar_fd('-', fd);
 			ft_putnbr_fd(-n, fd);
-		}
+	}
+	else if (n > 9)
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(n % 10, fd);
 	}
 	else
-	{
-		if (n > 9)
-			ft_putnbr_fd(n / 10, fd);
-		else
-			ft_putchar_fd('0' + n, fd);
-	}
+		ft_putchar_fd('0' + n, fd);
 }
