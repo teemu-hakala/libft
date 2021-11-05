2f81b8c23d8ef6b02ff2511b9a874f550efba26e
Teemu Hakala <thakala@c1r6p16.hive.fi>
2021-11-05 07:33:40 +0200

    ft_itoa debugged

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,19 +6,19 @@
 /*   By: thakala <thakala@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 16:03:12 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/04 10:41:35 by thakala          ###   ########.fr       */
+/*   Updated: 2021/11/05 07:32:11 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-static int	ft_intlen(int n)
+static size_t	ft_intlen(int n)
 {
-	int		l;
+	size_t		l;
 
 	l = 0;
-	if (n < 0)
+	if (n <= 0)
 		l++;
 	while (n)
 	{
@@ -31,20 +31,26 @@ static int	ft_intlen(int n)
 char	*ft_itoa(int n)
 {
 	char	*numeral;
-	int		n_len;
-	int		i;
+	size_t	n_len;
+	size_t	i;
 
 	n_len = ft_intlen(n);
 	numeral = (char *)malloc(sizeof(*numeral) * (n_len + 1));
 	if (!numeral)
 		return (0);
-	if (n < 0)
-		numeral[0] = '-';
 	i = n_len;
-	numeral[i--] = '\0';
-	while (i > 0)
+	numeral[i] = '\0';
+	if (n < 0)
 	{
-		numeral[i--] = '0' + n % 10;
+		numeral[0] = '-';
+		numeral[--i] = '0' + -(n % 10);
+		n /= -10;
+	}
+	else if (n == 0)
+		numeral[--i] = '0';
+	while (i > 0 && n)
+	{
+		numeral[--i] = '0' + n % 10;
 		n /= 10;
 	}
 	return (numeral);
