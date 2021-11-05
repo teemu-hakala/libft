57c68190de8fb55bce0a4a8ef93cf9602c0fc44e
Teemu Hakala <thakala@c1r6p16.hive.fi>
2021-11-05 11:33:30 +0200

    ft_memccpy n-restricted solution

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: thakala <thakala@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 19:51:48 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/04 20:18:38 by thakala          ###   ########.fr       */
+/*   Updated: 2021/11/05 08:05:09 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 	unsigned char	*destination;
 	unsigned char	*source;
 
-	while (*source && *source != (unsigned char)c)
+	while (*source && *source != (unsigned char)c && n--)
 		*destination++ = *source++;
 	*destination++ = *source;
 	if (*source)
