87571e87efd2cef4f762534d11ea478e8bcc18ca
Teemu Hakala <thakala@c1r6p16.hive.fi>
2021-11-06 15:01:22 +0200

    ft_strcat null protection

--- ft_strcat.c
+++ ft_strcat.c
@@ -6,14 +6,18 @@
 /*   By: thakala <thakala@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 11:57:20 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/03 16:01:44 by thakala          ###   ########.fr       */
+/*   Updated: 2021/11/06 15:01:02 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <string.h>
 
 char	*ft_strcat(char *s1, const char *s2)
 {
 	int		i;
 
+	if (!s1 || !s2)
+		return (NULL);
 	i = 0;
 	while (s1[i])
 		i++;
