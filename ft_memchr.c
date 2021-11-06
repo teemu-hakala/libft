7db7880eb6823d70f25f2590a99f00846603d70b
Teemu Hakala <thakala@c1r5p6.hive.fi>
2021-11-06 05:37:48 +0200

    ft_memchr removed dereferenciation

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,13 +6,13 @@
 /*   By: thakala <thakala@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 20:19:25 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/04 20:26:41 by thakala          ###   ########.fr       */
+/*   Updated: 2021/11/05 13:48:20 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memchar(const void *s, int c, size_t n)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
 	unsigned char	*string;
 
@@ -20,7 +20,7 @@ void	*ft_memchar(const void *s, int c, size_t n)
 	while (n-- && *string)
 	{
 		if (*string == (unsigned char)c)
-			return (*string);
+			return (string);
 		string++;
 	}
 	return (0);
