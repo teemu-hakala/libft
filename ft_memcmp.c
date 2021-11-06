d1842826badbcc230c3e026dab0d679473e244c6
Teemu Hakala <thakala@c1r6p16.hive.fi>
2021-11-06 11:07:46 +0200

    ft_memcmp preliminary

---
+++ ft_memcmp.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thakala <thakala@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/06 05:38:53 by thakala           #+#    #+#             */
+/*   Updated: 2021/11/06 11:06:49 by thakala          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	unsigned char	*string1;
+	unsigned char	*string2;
+
+	while (*string1 == *string2 && n--)
+	{
+		string1++;
+		string2++;
+	}
+	return (*string1 - *string2);
+}
