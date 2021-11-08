20c910f95f1090163ce8cfaf1448f034d252497b
Teemu Hakala <thakala@c1r6p16.hive.fi>
2021-11-08 17:06:47 +0200

    ft_memmove without malloc

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,19 +6,37 @@
 /*   By: thakala <thakala@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 14:35:21 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/08 14:48:27 by thakala          ###   ########.fr       */
+/*   Updated: 2021/11/08 17:04:34 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
 #include "libft.h"
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	char	*copy;
+	size_t	i;
 
-	copy = (char *)malloc(sizeof(char) * len);
-	if (!copy)
-		return (NULL);
-	return (ft_memcpy(dst, ft_memcpy(copy, src, len), len));
+	if (!len)
+		return (dst);
+	if (src < dst)
+	{
+		while (1)
+		{
+			((char *)dst)[len - 1] = ((char *)src)[len - 1];
+			if (len > 1)
+				len--;
+			else
+				break ;
+		}
+	}
+	else if (src > dst)
+	{
+		i = 0;
+		while (i < len)
+		{
+			((char *)dst)[i] = ((char *)src)[i];
+			i++;
+		}
+	}
+	return (dst);
 }
