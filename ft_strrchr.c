591adba259f59a0381b2bee9c6236dd15a9889fd
Teemu Hakala <thakala@c1r6p16.hive.fi>
2021-11-08 18:51:27 +0200

    ft_strrchr removed null protection

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: thakala <thakala@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/07 18:04:07 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/07 19:08:10 by thakala          ###   ########.fr       */
+/*   Updated: 2021/11/08 18:51:09 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ char	*ft_strrchr(const char *s, int c)
 {
 	const char	*result;
 
-	if (!s)
-		return (NULL);
 	result = NULL;
 	while (*s)
 		if (*s++ == (char)c)
