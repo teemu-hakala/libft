e3fee216287a2f033480ff77bbad2ee2aed75e71
Teemu Hakala <thakala@c1r6p16.hive.fi>
2021-11-11 09:01:46 +0200

    ft_memcpy avoid implicit casts

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: thakala <thakala@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 19:58:22 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/06 11:38:36 by thakala          ###   ########.fr       */
+/*   Updated: 2021/11/11 09:00:59 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,5 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	new_beginning = destination;
 	while (n--)
 		*destination++ = *source++;
-	return (new_beginning);
+	return ((void *)new_beginning);
 }
