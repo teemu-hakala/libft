3294aa3fbc2749d5b6651068044ba3cdb95ffb54
Teemu Hakala <thakala@c1r6p16.hive.fi>
2021-11-11 17:02:51 +0200

    ft_memcpy narrowed inclusions, mimicked behavior of libc function

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,11 +6,11 @@
 /*   By: thakala <thakala@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 19:58:22 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/11 09:00:59 by thakala          ###   ########.fr       */
+/*   Updated: 2021/11/11 11:02:39 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include <string.h>
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
@@ -18,6 +18,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	char	*source;
 	char	*new_beginning;
 
+	if (n == 0 || (!dst && !src))
+		return (dst);
 	destination = (char *)dst;
 	source = (char *)src;
 	new_beginning = destination;
