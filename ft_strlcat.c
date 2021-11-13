cb7f82ac7c7265fc67bc619beade80a875579c1c
Teemu Hakala <thakala@c1r6p16.hive.fi>
2021-11-13 16:49:09 +0200

    ft_strlcat rework to mimick libc behavior

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: thakala <thakala@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/06 15:38:59 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/08 18:50:15 by thakala          ###   ########.fr       */
+/*   Updated: 2021/11/13 16:41:16 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,20 @@
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	dst_len;
+	size_t	src_len;
+	size_t	i;
 
-	dst_len = 0;
-	while (*dst)
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	if (dst_len > dstsize)
+		return (src_len + dstsize);
+	i = (size_t)0;
+	while (src[i] && dst_len < dstsize && i < dstsize - dst_len - (size_t)1)
 	{
-		dst++;
-		dst_len++;
+		dst[dst_len + i] = src[i];
+		i++;
 	}
-	if (dstsize != 0 && dst_len < --dstsize)
-		dst[dstsize] = '\0';
-	while (dst_len < dstsize)
-	{
-		dst[dstsize - dst_len - 1] = src[dstsize - dst_len - 1];
-		dstsize--;
-	}
-	return (dst_len + ft_strlen(src));
+	if (dstsize > 0 && dst_len < dstsize)
+		dst[dst_len + i] = '\0';
+	return (dst_len + src_len);
 }
