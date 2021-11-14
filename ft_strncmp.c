2df0ec6f48d045e59171d505bc29ce04515b72d4
Teemu Hakala <thakala@c1r6p16.hive.fi>
2021-11-14 20:41:10 +0200

    ft_strncmp edge case of 0 length

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: thakala <thakala@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 16:41:30 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/10 16:53:40 by thakala          ###   ########.fr       */
+/*   Updated: 2021/11/14 20:25:42 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
+	if (!n)
+		return (0);
 	while (--n > 0 && *s1 == *s2 && *s1)
 	{
 		s1++;
