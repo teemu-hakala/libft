ba4f4350f3f88f7fc0d96693564ead28cae46245
Teemu Hakala <thakala@c1r6p16.hive.fi>
2021-11-17 20:00:24 +0200

    ft_strsub preliminary

--- ft_strsub.c
+++ ft_strsub.c
@@ -6,11 +6,12 @@
 /*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 12:05:10 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/17 15:43:25 by thakala          ###   ########.fr       */
+/*   Updated: 2021/11/17 19:58:53 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
+#include <stdlib.h>
 
 char	*ft_strsub(char const *s, unsigned int start, size_t len)
 {
@@ -22,6 +23,7 @@ char	*ft_strsub(char const *s, unsigned int start, size_t len)
 	if (!substr)
 		return (NULL);
 	zu_start = start;
+	i = 0;
 	while (i < len)
 		substr[i++] = s[zu_start++];
 	substr[i] = '\0';
