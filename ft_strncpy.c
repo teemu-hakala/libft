6792b226ebaf023510455e6788ad669888a4757e
Teemu Hakala <thakala@c3r5p4.hive.fi>
2021-11-20 19:22:11 +0200

    ft_strncpy remove null protection

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thakala <thakala@student.42.fr>            +#+  +:+       +#+        */
+/*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 16:37:45 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/06 14:30:02 by thakala          ###   ########.fr       */
+/*   Updated: 2021/11/20 19:14:34 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ char	*ft_strncpy(char *dst, const char *src, size_t len)
 {
 	char	*new_beginning;
 
-	if (!dst || !src)
-		return (NULL);
 	new_beginning = dst;
 	while (*src && len)
 	{
