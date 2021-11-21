b78abe2df281984956453f59e5fcd79684e65261
Teemu Hakala <thakala@c3r5p4.hive.fi>
2021-11-21 13:18:49 +0200

    ft_lstdel preliminary

--- ft_lstdel.c
+++ ft_lstdel.c
@@ -6,53 +6,9 @@
 /*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/20 19:55:30 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/21 13:16:50 by thakala          ###   ########.fr       */
+/*   Updated: 2021/11/21 13:18:25 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-/*#include "libft.h"
-#include <stdlib.h>
-
-void	ft_lstdel(t_list **alst, void (*del)(void*, size_t))
-{
-	if (!alst)
-		return ;
-	while (*alst != NULL)
-	{
-		del((*alst)->content, (*alst)->content_size);
-		free(*alst);
-		*alst = (*alst)->next;
-	}
-	*alst = NULL;
-}*/
-
-/*
-#include "libft.h"
-			#include <stdio.h>
-
-void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
-{
-	t_list	*upcoming;
-
-	while (*alst)
-	{
-		printf("*alst: %s\n", (char *)((*alst)->content));
-		printf("lstdel: %s\n", (char *)((*alst)->content));
-		upcoming = (*alst)->next;
-		if (upcoming)
-			printf("lstdel upcoming: %s\n", (char *)(upcoming->content));
-		else
-			printf("lstdel upcoming: %s\n", "NULL");
-		printf("*alst: %s\n", (char *)((*alst)->content));
-		if ((*alst)->content)
-		{
-			printf("lstdel: calling lstdelone\n");
-			ft_lstdelone(alst, del);
-		}
-		alst = &upcoming;
-	}
-	*alst = NULL;
-}*/
 
 #include "libft.h"
 
@@ -71,31 +27,3 @@ void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
 	}
 	*alst = NULL;
 }
-/*
-
-#include "libft.h"
-			#include <stdio.h>
-
-void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
-{
-	t_list	**upcoming;
-
-	while (*alst)
-	{
-		printf(" *alst: %s\n", (char *)((*alst)->content));
-		printf("lstdel: %s\n", (char *)((*alst)->content));
-		upcoming = &((*alst)->next);
-		if (*upcoming)
-			printf("lstdel upcoming: %s\n", (char *)((*upcoming)->content));
-		else
-			printf("lstdel upcoming: %s\n", "NULL");
-		printf("*alst: %s\n", (char *)((*alst)->content));
-		if ((*alst))
-		{
-			printf("lstdel: calling lstdelone\n");
-			ft_lstdelone(alst, del);
-		}
-		alst = upcoming;
-	}
-	*alst = NULL;
-}*/
