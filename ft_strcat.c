c6876139665ad70122ce584ad0424397024d7eed
Teemu Hakala <thakala@c3r5p4.hive.fi>
2021-11-21 19:24:29 +0200

    ft_strcat pretty crossplay

--- ft_strcat.c
+++ ft_strcat.c
@@ -6,21 +6,14 @@
 /*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 11:57:20 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/21 19:07:04 by thakala          ###   ########.fr       */
+/*   Updated: 2021/11/21 19:24:08 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
+#include "libft.h"
 
 char	*ft_strcat(char *s1, const char *s2)
 {
-	size_t	i;
-
-	i = 0;
-	while (s1[i])
-		i++;
-	while (*s2)
-		s1[i++] = *s2++;
-	s1[i] = *s2;
+	ft_strcpy(&(s1[ft_strlen(s1)]), s2);
 	return (s1);
 }
