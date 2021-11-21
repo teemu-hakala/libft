4d771d410f4a9e86bc1814cad06df4a381631d01
Teemu Hakala <thakala@c3r5p4.hive.fi>
2021-11-21 19:06:03 +0200

    ft_strncpy narrower inclusion

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -6,11 +6,11 @@
 /*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 16:37:45 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/20 19:14:34 by thakala          ###   ########.fr       */
+/*   Updated: 2021/11/21 19:05:40 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include <string.h>
 
 char	*ft_strncpy(char *dst, const char *src, size_t len)
 {
