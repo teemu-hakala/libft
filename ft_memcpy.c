20312c5f69a3be0dbe2aafb598db1f24af7e61a3
Teemu Hakala <thakala@c3r5p4.hive.fi>
2021-11-21 17:51:27 +0200

    ft_memcpy pretty

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thakala <thakala@student.42.fr>            +#+  +:+       +#+        */
+/*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 19:58:22 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/11 11:02:39 by thakala          ###   ########.fr       */
+/*   Updated: 2021/11/21 17:50:46 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,10 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	char	*destination;
-	char	*source;
-	char	*new_beginning;
-
 	if (n == 0 || (!dst && !src))
 		return (dst);
-	destination = (char *)dst;
-	source = (char *)src;
-	new_beginning = destination;
+	n++;
 	while (n--)
-		*destination++ = *source++;
-	return ((void *)new_beginning);
+		((unsigned char *)dst)[n - 1] = ((unsigned char *)src)[n - 1];
+	return (dst);
 }
