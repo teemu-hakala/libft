bda7a5b6f5b5204401d4f8db0f6c419d7a7e77b4
Teemu Hakala <thakala@c1r6p14.hive.fi>
2021-11-23 19:44:40 +0200

    ft_atoi crossplay (M)

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,9 +6,11 @@
 /*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 12:19:47 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/20 11:43:23 by thakala          ###   ########.fr       */
+/*   Updated: 2021/11/23 19:44:09 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 int	ft_atoi(const char *str)
 {
@@ -26,7 +28,7 @@ int	ft_atoi(const char *str)
 		str++;
 	}
 	result = 0;
-	while (*str >= '0' && *str <= '9')
+	while (ft_isdigit(*str))
 		result = 10 * result + *str++ - '0';
 	return (coefficient * result);
 }
