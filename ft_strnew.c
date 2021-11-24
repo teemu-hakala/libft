e4919e4db0e4cdfa202bc6cf8614dc6d0adb742e
Teemu Hakala <thakala@c1r6p14.hive.fi>
2021-11-24 19:38:44 +0200

    ft_strnew concise, crossplay

--- ft_strnew.c
+++ ft_strnew.c
@@ -3,14 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strnew.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thakala <thakala@student.42.fr>            +#+  +:+       +#+        */
+/*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 17:59:09 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/10 18:23:09 by thakala          ###   ########.fr       */
+/*   Updated: 2021/11/24 19:37:30 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
+#include "libft.h"
 #include <stdlib.h>
 
 char	*ft_strnew(size_t size)
@@ -18,9 +18,7 @@ char	*ft_strnew(size_t size)
 	char	*s;
 
 	s = (char *)malloc(sizeof(char) * (++size));
-	if (!s)
-		return (NULL);
-	while (size)
-		s[size-- - 1] = '\0';
+	if (s)
+		ft_bzero(s, size);
 	return (s);
 }
