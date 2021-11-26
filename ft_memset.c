ed08c7deb623ee1a5796f271c4c79e10d05d0d0e
Teemu Hakala <thakala@c1r6p14.hive.fi>
2021-11-26 11:28:30 +0200

    ft_memset conscise, performant, M

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 10:58:37 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/21 17:40:05 by thakala          ###   ########.fr       */
+/*   Updated: 2021/11/26 11:25:42 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	*ft_memset(void *b, int c, size_t len)
 {
-	while (len)
-		((unsigned char *)b)[len-- - 1] = (unsigned char)c;
+	while (len--)
+		*((unsigned char *)b++) = (unsigned char)c;
 	return (b);
 }
