fa007eb13bd17a0de7f518f50d5737d0f5a98b53
Teemu Hakala <thakala@c1r6p14.hive.fi>
2021-11-26 12:05:06 +0200

    ft_memcmp pretty, concise, performant

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/06 05:38:53 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/21 19:09:04 by thakala          ###   ########.fr       */
+/*   Updated: 2021/11/26 12:04:39 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,8 @@
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	if (!n)
-		return (n);
-	while (--n && *((unsigned char *)s1) == *((unsigned char *)s2))
-	{
-		s1++;
-		s2++;
-	}
-	return ((int)(*((unsigned char *)s1) - *((unsigned char *)s2)));
+		return (0);
+	while (*((unsigned char *)s1++) == *((unsigned char *)s2++) && --n)
+		;
+	return ((int)(*((unsigned char *)s1 - 1) - *((unsigned char *)s2 - 1)));
 }
