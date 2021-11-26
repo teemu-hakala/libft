96338dd28284cf57f5e8025cc0d1963a65a0cd76
Teemu Hakala <thakala@c1r6p14.hive.fi>
2021-11-26 20:22:53 +0200

    ft_atoi short, jraivio, extreme

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 12:19:47 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/26 20:17:41 by thakala          ###   ########.fr       */
+/*   Updated: 2021/11/26 20:22:12 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,9 @@ int	ft_atoi(const char *str)
 	int		result;
 
 	while (ft_isspace(*str))
-		str++;
-	coefficient = 1;
+		result = !str++;
 	if (*str == '+' || *str == '-')
 		coefficient = -(*str++ - 44);
-	result = 0;
 	while (ft_isdigit(*str))
 		result = 10 * result + *str++ - '0';
 	return (coefficient * result);
