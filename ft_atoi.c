6bfb1d52099c7bc7844c6432d49afb0ea9e9674b
Teemu Hakala <thakala@c1r6p14.hive.fi>
2021-11-26 20:28:32 +0200

    ft_atoi pretty, concise, jraivio, reverted

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 12:19:47 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/26 20:23:59 by thakala          ###   ########.fr       */
+/*   Updated: 2021/11/26 20:17:41 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ int	ft_atoi(const char *str)
 
 	while (ft_isspace(*str))
 		str++;
+	coefficient = 1;
 	if (*str == '+' || *str == '-')
 		coefficient = -(*str++ - 44);
 	result = 0;
