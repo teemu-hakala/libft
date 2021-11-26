2321e660488b34a8969251bb3221ecefee5625bb
Teemu Hakala <thakala@c1r6p14.hive.fi>
2021-11-26 18:34:50 +0200

    ft_strnequ short

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strnequ.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thakala <thakala@student.42.fr>            +#+  +:+       +#+        */
+/*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/14 18:57:58 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/14 20:40:25 by thakala          ###   ########.fr       */
+/*   Updated: 2021/11/26 18:33:36 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,7 @@ int	ft_strnequ(char const *s1, char const *s2, size_t n)
 {
 	if (!n)
 		return (1);
-	while (*s1 && *s1 == *s2 && --n > 0)
-	{
-		s1++;
-		s2++;
-	}
-	return (*s1 == *s2);
+	while (*s1++ == *s2++ && *(s1 - 1) && --n > 0)
+		;
+	return (*(s1 - 1) == *(s2 - 1));
 }
