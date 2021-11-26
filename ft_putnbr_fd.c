d965291b407fa75d60a3905b39afdd10ebc1dd8a
Teemu Hakala <thakala@c1r6p14.hive.fi>
2021-11-26 22:23:08 +0200

    ft_putnbr_fd branchless, ingenious, curious

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thakala <thakala@student.42.fr>            +#+  +:+       +#+        */
+/*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 09:30:17 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/04 17:19:37 by thakala          ###   ########.fr       */
+/*   Updated: 2021/11/26 22:18:00 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <unistd.h>
 
-void	ft_putnbr_fd(int n, int fd)
+void	ft_putnbr_fd_0(int n, int fd)
 {
 	char	d;
 
@@ -38,4 +38,66 @@ void	ft_putnbr_fd(int n, int fd)
 		d = '0' + (char)n;
 		write(fd, &d, 1);
 	}
+}
+
+/*
+	jturunen
+*/
+
+void	ft_putnbr_fd_10(int n, int fd)
+{
+	if (n / 10 != 0)
+		ft_putnbr_fd(n / 10, fd);
+	else if (n < 0)
+		ft_putchar_fd('-', fd);
+	if (n < 0)
+		ft_putchar_fd(((n % 10) * -1) + '0', fd);
+	else
+		ft_putchar_fd((n % 10) + '0', fd);
+}
+
+void	ft_putnbr_fd_11(int n, int fd)
+{
+	if (n / 10)
+		ft_putnbr_fd(n / 10, fd);
+	else if (n < 0)
+		ft_putchar_fd('-', fd);
+	if (n < 0)
+		ft_putchar_fd(-(n % 10) + '0', fd);
+	else
+		ft_putchar_fd((n % 10) + '0', fd);
+}
+
+/*
+	jraivio
+*/
+
+void	ft_putnbr_fd_20(int n, int fd)
+{
+	if (n < -9 || n > 9)
+		ft_putnbr_fd(n / 10, fd);
+	else if (n >= -9 && n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		ft_putchar_fd((char)(n * -1) + '0', fd);
+		return ;
+	}
+	n %= 10;
+	if (n < 0)
+		n *= -1;
+	ft_putchar_fd((char)n + '0', fd);
+}
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	if (n < -9 || n > 9)
+		ft_putnbr_fd(n / 10, fd);
+	else if (n >= -9 && n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		return (ft_putchar_fd((char)(-n) + '0', fd));
+	}
+	n %= 10;
+	n *= (-(n < 0)) | 0x1;
+	ft_putchar_fd((char)n + '0', fd);
 }
