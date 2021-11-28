a558abf755c84628371638633aed9a96b94e5262
Teemu Hakala <thakala@c1r6p14.hive.fi>
2021-11-28 12:53:35 +0200

    ft_strsep preliminary

---
+++ ft_strsep.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsep.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/28 12:41:27 by thakala           #+#    #+#             */
+/*   Updated: 2021/11/28 12:51:56 by thakala          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strsep(char **stringp, const char *delim)
+{
+	char	*beginning;
+
+	beginning = *stringp;
+	if (!beginning)
+		return (beginning);
+	while (*stringp && *stringp != delim)
+		*stringp++;
+	*stringp = '\0';
+	return (beginning);
+}
