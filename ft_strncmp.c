35522290a9605528224f6191c23384d4bee88974
Teemu Hakala <thakala@c1r6p14.hive.fi>
2021-11-28 17:58:34 +0200

    ft_strncmp short

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thakala <thakala@student.42.fr>            +#+  +:+       +#+        */
+/*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 16:41:30 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/14 20:25:42 by thakala          ###   ########.fr       */
+/*   Updated: 2021/11/28 17:57:30 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	if (!n)
 		return (0);
-	while (--n > 0 && *s1 == *s2 && *s1)
-	{
-		s1++;
-		s2++;
-	}
-	return ((unsigned char)*s1 - (unsigned char)*s2);
+	while (*s1++ == *s2++ && *(s1 - 1) && --n > 0)
+		;
+	return ((unsigned char)*(s1 - 1) - (unsigned char)*(s2 - 1));
 }
