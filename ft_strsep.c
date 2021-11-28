f9b1ccbbe27efd6be2bb09b4671e1aee7166f1ea
Teemu Hakala <thakala@c1r6p14.hive.fi>
2021-11-28 13:43:06 +0200

    ft_strsep crossplay ft_strncmp

--- ft_strsep.c
+++ ft_strsep.c
@@ -6,18 +6,22 @@
 /*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/28 12:41:27 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/28 12:54:05 by thakala          ###   ########.fr       */
+/*   Updated: 2021/11/28 13:39:28 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <string.h>
 
 char	*ft_strsep(char **stringp, const char *delim)
 {
 	char	*beginning;
+	size_t	l_delim;
 
 	beginning = *stringp;
 	if (!beginning)
 		return (beginning);
-	while (*stringp && *stringp != delim)
+	l_delim = ft_strlen(delim);
+	while (**stringp && ft_strncmp(*stringp, delim, l_delim))
 		*stringp++;
 	*stringp++ = '\0';
 	return (beginning);
