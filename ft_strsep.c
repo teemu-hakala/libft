ed7a2a46fb3247258e5ff9d7a0f914e25d15e5ca
Teemu Hakala <thakala@c1r6p14.hive.fi>
2021-11-28 16:06:35 +0200

    ft_strsep good idea, not standard though

--- ft_strsep.c
+++ ft_strsep.c
@@ -6,11 +6,17 @@
 /*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/28 12:41:27 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/28 13:39:28 by thakala          ###   ########.fr       */
+/*   Updated: 2021/11/28 16:06:30 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
+#include "libft.h"
+
+/*
+	Treat delim as a delimiter string, which is to be skipped entirely,
+	without returning empty strings in between.
+	(Status quo is not representative.)
+*/
 
 char	*ft_strsep(char **stringp, const char *delim)
 {
@@ -22,7 +28,10 @@ char	*ft_strsep(char **stringp, const char *delim)
 		return (beginning);
 	l_delim = ft_strlen(delim);
 	while (**stringp && ft_strncmp(*stringp, delim, l_delim))
-		*stringp++;
-	*stringp++ = '\0';
+		(*stringp)++;
+	if (!**stringp)
+		*stringp = NULL;
+	if (*stringp)
+		*(*stringp)++ = '\0';
 	return (beginning);
 }
