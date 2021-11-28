11661bf0389e3b3de0c034b22528893bf877366d
Teemu Hakala <thakala@c1r6p14.hive.fi>
2021-11-28 12:37:28 +0200

    ft_strpbrk inner loop counter reset

--- ft_strpbrk.c
+++ ft_strpbrk.c
@@ -6,7 +6,7 @@
 /*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/28 12:22:39 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/28 12:32:56 by thakala          ###   ########.fr       */
+/*   Updated: 2021/11/28 12:36:14 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,13 @@ char	*ft_strpbrk(const char *s, const char *charset)
 {
 	size_t	c;
 
-	c = 0;
 	--s;
 	while (*++s)
+	{
+		c = 0;
 		while (charset[c])
 			if (charset[c++] == *s)
 				return (s);
+	}
 	return (NULL);
 }
