b522a80030ba01d63c3b730dec110528111f87ac
Teemu Hakala <thakala@c1r6p14.hive.fi>
2021-11-28 12:56:37 +0200

    ft_strsep skip after the delimiter returning a pointer to location not reserved necessarily for the string, caller function must handle this?

--- ft_strsep.c
+++ ft_strsep.c
@@ -6,7 +6,7 @@
 /*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/28 12:41:27 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/28 12:51:56 by thakala          ###   ########.fr       */
+/*   Updated: 2021/11/28 12:54:05 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,6 @@ char	*ft_strsep(char **stringp, const char *delim)
 		return (beginning);
 	while (*stringp && *stringp != delim)
 		*stringp++;
-	*stringp = '\0';
+	*stringp++ = '\0';
 	return (beginning);
 }
