900bea8af6924fd61823bfc7f3b0ac4efd12f1e0
Teemu Hakala <thakala@c1r6p14.hive.fi>
2021-11-29 23:39:37 +0200

    ft_strlcpy final

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/29 22:07:04 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/29 22:23:18 by thakala          ###   ########.fr       */
+/*   Updated: 2021/11/29 23:37:01 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,13 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	size_t	l_src;
 
 	l_src = 0;
-	while (--dstsize && src[l_src])
-		*dst++ = src[l_src++];
-	*dst = '\0';
+	if (dstsize)
+	{
+		while (--dstsize && src[l_src])
+			*dst++ = src[l_src++];
+		*dst = '\0';
+	}
+	while (src[l_src])
+		l_src++;
 	return (l_src);
 }
