441071495fd12fd1e3b6118982ac8be94218411d
Teemu Hakala <thakala@c1r6p14.hive.fi>
2021-12-04 19:15:23 +0200

    ft_memmove concise

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 14:35:21 by thakala           #+#    #+#             */
-/*   Updated: 2021/12/04 19:12:27 by thakala          ###   ########.fr       */
+/*   Updated: 2021/12/04 19:14:27 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,8 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 		return (dst);
 	i = (size_t)(-1);
 	if (src < dst)
-	{
-		while (1)
-		{
-			((char *)dst)[len - 1] = ((char *)src)[len - 1];
-			if (len > 1)
-				len--;
-			else
-				break ;
-		}
-	}
+		while (--len + 1)
+			((char *)dst)[len] = ((char *)src)[len];
 	else if (src > dst)
 		while (++i < len)
 			((char *)dst)[i] = ((char *)src)[i];
