fe58ede25f2d2aab11f9cd1214e731183d889d81
Teemu Hakala <thakala@c1r6p14.hive.fi>
2021-12-05 13:52:47 +0200

    ft_strsub idea into fruition

--- ft_strsub.c
+++ ft_strsub.c
@@ -6,7 +6,7 @@
 /*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 12:05:10 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/26 18:46:02 by thakala          ###   ########.fr       */
+/*   Updated: 2021/12/05 13:51:05 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@ char	*ft_strsub(char const *s, unsigned int start, size_t len)
 	substr = (char *)malloc(sizeof(char) * (len + 1));
 	if (!substr)
 		return (NULL);
-	zu_start = start;
+	zu_start = start + len;
 	substr[len] = '\0';
 	while (--len + 1)
-		substr[len] = s[zu_start + len];
+		substr[len] = s[--zu_start];
 	return (substr);
 }
