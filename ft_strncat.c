a45fa9c63464d4625466735d0cfea00325fb4d3c
Teemu Hakala <thakala@c1r6p14.hive.fi>
2021-12-05 13:39:59 +0200

    ft_strncat revert

--- ft_strncat.c
+++ ft_strncat.c
@@ -6,7 +6,7 @@
 /*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/06 14:55:34 by thakala           #+#    #+#             */
-/*   Updated: 2021/12/05 13:27:48 by thakala          ###   ########.fr       */
+/*   Updated: 2021/12/05 13:39:44 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,11 @@
 
 char	*ft_strncat(char *s1, const char *s2, size_t n)
 {
-	s1 += ft_strlen(s1);
+	size_t	i;
+
+	i = ft_strlen(s1);
 	while (*s2 && n--)
-		*s1++ = *s2++;
-	*s1 = '\0';
+		s1[i++] = *s2++;
+	s1[i] = '\0';
 	return (s1);
 }
