eeb71eabb65ee3f0734509d1139b28af285f2fe4
Teemu Hakala <thakala@c1r6p14.hive.fi>
2021-12-05 13:42:20 +0200

    ft_strncmp concise

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 16:41:30 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/28 17:57:30 by thakala          ###   ########.fr       */
+/*   Updated: 2021/12/05 13:42:09 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	if (!n)
 		return (0);
-	while (*s1++ == *s2++ && *(s1 - 1) && --n > 0)
+	while (*s1++ == *s2++ && *(s1 - 1) && --n)
 		;
 	return ((unsigned char)*(s1 - 1) - (unsigned char)*(s2 - 1));
 }
