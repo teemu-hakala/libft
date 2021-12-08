56351befd7a974362d83ee39b58de750744ec773
Teemu Hakala <thakala@c1r6p14.hive.fi>
2021-12-08 11:05:52 +0200

    ft_lstmap terminology, refactored

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,17 +6,17 @@
 /*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/21 13:35:44 by thakala           #+#    #+#             */
-/*   Updated: 2021/11/21 16:54:55 by thakala          ###   ########.fr       */
+/*   Updated: 2021/12/07 14:57:14 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-static void	ft_lstadd_back(t_list **alst, t_list *new)
+static void	ft_lstadd_back(t_list **current, t_list *additional)
 {
-	(*alst)->next = new;
-	*alst = new;
+	(*current)->next = additional;
+	*current = additional;
 }
 
 static void	ft_erase_node(void *content, size_t content_size)
@@ -27,25 +27,25 @@ static void	ft_erase_node(void *content, size_t content_size)
 
 t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
-	t_list	*new_beginning;
-	t_list	*new;
+	t_list	*head;
+	t_list	*current;
 	t_list	*additional;
 
-	new = f(lst);
-	if (!new)
+	current = f(lst);
+	if (!current)
 		return (NULL);
-	new_beginning = new;
+	head = current;
 	lst = lst->next;
 	while (lst)
 	{
 		additional = f(lst);
 		if (!additional)
 		{
-			ft_lstdel(&new_beginning, ft_erase_node);
+			ft_lstdel(&head, ft_erase_node);
 			return (NULL);
 		}
-		ft_lstadd_back(&new, additional);
+		ft_lstadd_back(&current, additional);
 		lst = lst->next;
 	}
-	return (new_beginning);
+	return (head);
 }
