e3a3c89e044012e6051a439f9bc24729ca51c2d6
Teemu Hakala <thakala@c1r6p16.hive.fi>
2021-12-12 17:26:43 +0200

    ft_strlcat concise

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/06 15:38:59 by thakala           #+#    #+#             */
-/*   Updated: 2021/12/05 13:24:15 by thakala          ###   ########.fr       */
+/*   Updated: 2021/12/12 17:21:30 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,14 @@
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	l_dst;
-	size_t	l_src;
 
 	l_dst = ft_strlen(dst);
-	l_src = 0;
 	if (l_dst >= dstsize)
 		return (ft_strlen(src) + dstsize);
-	while (*src && dstsize - (l_dst + l_src) - 1)
-		dst[l_dst + l_src++] = *src++;
-	dst[l_dst + l_src] = '\0';
+	while (*src && dstsize - l_dst - 1)
+		dst[l_dst++] = *src++;
+	dst[l_dst] = '\0';
 	while (*src++)
-		l_src++;
-	return (l_dst + l_src);
+		l_dst++;
+	return (l_dst);
 }
