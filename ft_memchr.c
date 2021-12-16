e62d4bae23b0309bbaeb20d59a58acd4bb8ec8e6
Teemu Hakala <thakala@c1r6p14.hive.fi>
2021-12-16 20:21:51 +0200

    ft_memchr concise

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 20:19:25 by thakala           #+#    #+#             */
-/*   Updated: 2021/12/04 19:09:41 by thakala          ###   ########.fr       */
+/*   Updated: 2021/12/16 20:01:57 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,8 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t	i;
-
-	i = (size_t)(-1);
-	while (++i < n)
-		if (((unsigned char *)s)[i] == (unsigned char)c)
-			return ((void *)&(s[i]));
+	while (n--)
+		if (*((unsigned char *)s++) == (unsigned char)c)
+			return ((void *)(s - 1));
 	return (NULL);
 }
